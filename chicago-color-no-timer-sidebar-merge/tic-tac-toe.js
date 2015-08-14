0157ca03f76212cb8fe3a33cd4c0935b6fe7e9d7
Jeff <happyguitar@gmail.com>
2015-08-14 03:43:24 -0500

    Change colors with timer function

--- chicago-color-no-timer-sidebar-merge/tic-tac-toe.js
+++ chicago-color-no-timer-sidebar-merge/tic-tac-toe.js
@@ -43,7 +43,7 @@ function showAnswer (e) {
   var tile = e.target;
   
   // Tile stays blue if it's already blue
-  if (tile.className == "blue-background") {
+  if (tile.className === "blue-background") {
     tile.className = "blue-background";
   } else {
     selectedTile.className = ""; // deletes all classes
@@ -72,6 +72,37 @@ function flashText() {
 
     oElem = document.getElementById(year);
     oElem.className = "blue-background";
+
+    if (year === 1910) {
+      map.series.regions[0].setValues(chicago1910colors());
+    }
+    if (year === 1920) {
+      map.series.regions[0].setValues(chicago1920colors());
+    }
+    if (year === 1930) {
+      map.series.regions[0].setValues(chicago1930colors());
+    }
+    if (year === 1940) {
+      map.series.regions[0].setValues(chicago1940colors());
+    }
+    if (year === 1950) {
+      map.series.regions[0].setValues(chicago1950colors());
+    }
+    if (year === 1960) {
+      map.series.regions[0].setValues(chicago1960colors());
+    }
+    if (year === 1970) {
+      map.series.regions[0].setValues(chicago1970colors());
+    }
+    if (year === 1980) {
+      map.series.regions[0].setValues(chicago1980colors());
+    }
+    if (year === 1990) {
+      map.series.regions[0].setValues(chicago1990colors());
+    }
+    if (year === 2000) {
+      map.series.regions[0].setValues(chicago2000colors());
+    }
 
 
     previousYear = year;
